07c1e55482fc0a15372c7b995072e2c398a3bb00
Nikhil Nigade <nikhil@dezinezync.com>
2019-10-08 12:05:21 +0530

    added image loading example

---
+++ Example/DZNetworking Example/DZNetworking Example/ImageController.h
@@ -0,0 +1,17 @@
+//
+//  ImageController.h
+//  DZNetworking Example
+//
+//  Created by Nikhil Nigade on 08/10/19.
+//  Copyright © 2019 Dezine Zync Studios LLP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImageController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
